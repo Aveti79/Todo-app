339cdfba62b243beb5af22110959972671baf603
Lukasz Ptaszek <lukasz.ptaszek@interia.pl>
2022-03-26 14:12:13 +0100

    *Added TaskGroupRepository interface *Added SqlTaskGroupRepository with HQL Query *Moved SQL interfaces to adapter package

---
+++ src/main/java/com/kodart/todoapp/adapter/SqlTaskGroupRepository.java
@@ -0,0 +1,17 @@
+package com.kodart.todoapp.adapter;
+
+import com.kodart.todoapp.model.TaskGroup;
+import com.kodart.todoapp.model.TaskGroupRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
+
+    @Override
+    @Query("from TaskGroup g join fetch g.tasks")
+    List<TaskGroup> findAll();
+}
