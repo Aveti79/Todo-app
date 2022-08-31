1650b3eb9cb010af808889c771b08281b7a516e1
Lukasz Ptaszek <lukasz.ptaszek@interia.pl>
2022-08-31 16:47:18 +0200

    Added endpoint /done-before-dd/{id} to check if task was done before deadline.

--- src/main/java/com/kodart/todoapp/controller/IllegalExceptionsProcessing.java
+++ src/main/java/com/kodart/todoapp/controller/IllegalExceptionsProcessing.java
@@ -9,5 +9,5 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@interface IllegalExceptionsProcessing {
+public @interface IllegalExceptionsProcessing {
 }
--- src/main/java/com/kodart/todoapp/reports/ReportController.java
+++ src/main/java/com/kodart/todoapp/reports/ReportController.java
@@ -1,16 +1,21 @@
 package com.kodart.todoapp.reports;
 
+import com.kodart.todoapp.controller.IllegalExceptionsProcessing;
 import com.kodart.todoapp.model.Task;
 import com.kodart.todoapp.model.TaskRepository;
+import com.kodart.todoapp.model.event.TaskDone;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
+@IllegalExceptionsProcessing
 @RequestMapping("/reports")
 public class ReportController {
 
@@ -30,6 +35,14 @@ public class ReportController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @GetMapping("/done-before-dd/{id}")
+    ResponseEntity<TaskDoneBeforeDeadline> checkTaskDoneTime(@PathVariable int id) {
+        return taskRepository.findById(id)
+                .map(task -> new TaskDoneBeforeDeadline(task, eventRepository.findByTaskId(id)))
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new IllegalStateException("There's no changes records for this task Id."));
+    }
+
     private static class TaskWithChangesCount {
 
         public String description;
@@ -40,6 +53,27 @@ public class ReportController {
             description = task.getDescription();
             done = task.isDone();
             changesCount = events.size();
+        }
+    }
+
+
+    private static class TaskDoneBeforeDeadline {
+
+        public LocalDateTime lastChangeTime;
+        public LocalDateTime deadline;
+        public boolean isDoneBeforeDeadline;
+
+        public TaskDoneBeforeDeadline(final Task task, final List<PersistedTaskEvent> events) {
+            if (task.isDone() && task.getDeadline() != null) {
+                lastChangeTime = events.stream()
+                        .filter(persistedTaskEvent -> persistedTaskEvent.name.equals(TaskDone.class.getSimpleName()))
+                        .map(persistedTaskEvent -> persistedTaskEvent.occurrence).max(Comparator.naturalOrder())
+                        .orElseThrow(() -> new IllegalStateException("There wasn't any changes before"));
+
+                deadline = task.getDeadline();
+                isDoneBeforeDeadline = lastChangeTime.isBefore(deadline);
+            }
+            else this.isDoneBeforeDeadline = task.isDone();
         }
     }
 }
