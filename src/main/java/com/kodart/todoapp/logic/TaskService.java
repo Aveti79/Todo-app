08a1289e89763c0fd93e0c6a64492042408eda53
Lukasz Ptaszek <lukasz.ptaszek@interia.pl>
2023-01-02 16:20:49 +0100

    Added Method to sort Tasks by deadline, and documentation for other method.

--- src/main/java/com/kodart/todoapp/logic/TaskService.java
+++ src/main/java/com/kodart/todoapp/logic/TaskService.java
@@ -5,12 +5,12 @@ import com.kodart.todoapp.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
-@Service
 public class TaskService {
     private static final Logger logger = LoggerFactory.getLogger(TaskService.class);
     private final TaskRepository repository;
@@ -19,9 +19,23 @@ public class TaskService {
         this.repository = repository;
     }
 
+    /**
+     * Odczytanie pełnej listy tasków, może potencjalnie zając więcej czasu,
+     * dlatego w tym przypadku została zastosowana asynchronczność.
+     */
+
     @Async
     public CompletableFuture<List<Task>> findAllAsync() {
         logger.info("Supply async");
-        return CompletableFuture.supplyAsync(repository::findAll);
+        return CompletableFuture.supplyAsync(this::getTaskList);
+    }
+
+    private List<Task> getTaskList() {
+        List<Task> tasks = repository.findAll()
+                .stream()
+                .sorted(Comparator.comparing(Task::getDeadline,
+                        Comparator.nullsFirst(Comparator.naturalOrder())))
+                .collect(Collectors.toList());
+        return tasks;
     }
 }
