73bfe1b7ebe8083f079d1b5cb1d1a868c9a3b537
Lukasz Ptaszek <lukasz.ptaszek@interia.pl>
2023-01-02 15:25:34 +0100

    Added Keycloak security module with configuration also added option to easily disable it for testing purposes.

--- src/main/java/com/kodart/todoapp/security/SecurityConfiguration.java
+++ src/main/java/com/kodart/todoapp/security/SecurityConfiguration.java
@@ -1,19 +1,25 @@
 package com.kodart.todoapp.security;
 
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
+@EnableGlobalMethodSecurity(
+        jsr250Enabled = true,
+        securedEnabled = true,
+        prePostEnabled = true)
 @KeycloakConfiguration
+@ConditionalOnProperty(name = "keycloak.enabled", havingValue = "true", matchIfMissing = true)
 public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
 
     @Autowired
@@ -34,9 +40,10 @@ public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         super.configure(http);
+        http.cors().and().csrf().disable();
+        http.sessionManagement()
+                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
         http.authorizeRequests()
-                .antMatchers("/info/*")
-                .hasRole("USER")
                 .anyRequest()
                 .permitAll();
     }
