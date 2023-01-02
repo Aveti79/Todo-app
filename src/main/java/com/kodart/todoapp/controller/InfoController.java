83badbb9ffcc056d223e57bdfd7d2ff52f9e7476
Lukasz Ptaszek <lukasz.ptaszek@interia.pl>
2023-01-02 16:03:51 +0100

    Added user verification to InfoController.java endpoints

--- src/main/java/com/kodart/todoapp/controller/InfoController.java
+++ src/main/java/com/kodart/todoapp/controller/InfoController.java
@@ -2,6 +2,7 @@ package com.kodart.todoapp.controller;
 
 import com.kodart.todoapp.TaskConfigurationProperties;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,11 +22,13 @@ public class InfoController {
         this.myProp = myProp;
     }
 
+    @Secured("ROLE_ADMIN")
     @GetMapping("/url")
     String url() {
         return dataSource.getUrl();
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
     @GetMapping("/prop")
     boolean myProp() {
         return myProp.getTemplate().isAllowMultipleTasks();
