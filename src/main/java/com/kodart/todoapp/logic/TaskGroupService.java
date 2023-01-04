7f6d77d0f693030c01d44c54efaab9803993026c
Lukasz Ptaszek <lukasz.ptaszek@interia.pl>
2023-01-04 16:57:39 +0100

    Refactor of fields names in TaskGroupService, delete Spring @Service annotation, add TaskGroupRepository to constructor and use it in methods where it was needed.

--- src/main/java/com/kodart/todoapp/logic/TaskGroupService.java
+++ src/main/java/com/kodart/todoapp/logic/TaskGroupService.java
@@ -1,31 +1,35 @@
 package com.kodart.todoapp.logic;
 
+import com.kodart.todoapp.model.Project;
 import com.kodart.todoapp.model.TaskGroup;
 import com.kodart.todoapp.model.TaskGroupRepository;
 import com.kodart.todoapp.model.TaskRepository;
 import com.kodart.todoapp.model.projection.GroupReadModel;
-import com.kodart.todoapp.model.projection.GroupWirteModel;
-import org.springframework.stereotype.Service;
+import com.kodart.todoapp.model.projection.GroupWriteModel;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Service
 public class TaskGroupService {
-    private TaskGroupRepository repository;
+    private TaskGroupRepository taskGroupRepository;
     private TaskRepository taskRepository;
 
-    TaskGroupService(final TaskGroupRepository repository) {
-        this.repository = repository;
+    TaskGroupService(final TaskGroupRepository taskGroupRepository, final TaskRepository taskRepository) {
+        this.taskGroupRepository = taskGroupRepository;
+        this.taskRepository = taskRepository;
     }
 
-    public GroupReadModel createGroup(GroupWirteModel source) {
-        TaskGroup result = repository.save(source.toGroup());
+    public GroupReadModel createGroup(GroupWriteModel source) {
+        return createGroup(source, null);
+    }
+
+    GroupReadModel createGroup(final GroupWriteModel source, final Project project) {
+        TaskGroup result = taskGroupRepository.save(source.toGroup(project));
         return new GroupReadModel(result);
     }
 
     public List<GroupReadModel> readAll() {
-        return repository.findAll().stream()
+        return taskGroupRepository.findAll().stream()
                 .map(GroupReadModel::new)
                 .collect(Collectors.toList());
     }
@@ -34,7 +38,8 @@ public class TaskGroupService {
         if(taskRepository.existsByDoneIsFalseAndGroup_Id(groupId)) {
             throw new IllegalStateException("You need to get done all tasks in group before group closing.");
         }
-        TaskGroup result = repository.findById(groupId).orElseThrow(() -> new IllegalArgumentException("TaskGroup with given Id doesn't exists."));
+        TaskGroup result = taskGroupRepository.findById(groupId).orElseThrow(() -> new IllegalArgumentException("TaskGroup with given Id doesn't exists."));
         result.setDone(!result.isDone());
+        taskGroupRepository.save(result);
     }
 }
